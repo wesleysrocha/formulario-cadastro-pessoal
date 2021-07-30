95a4dd3125c8e910244c0f33d651cedb4a633905
wesleysrocha <wesleyrocha.it19@gmail.com>
2021-07-29 22:31:25 -0300

    atualizacões finais e add mascaras

---
+++ mascara-de-validacao.js
@@ -0,0 +1,8 @@
+//mascara celular  somente números 11 digitos sem espaço e "-"
+$("#celular").mask("00000000009");
+//mascara telefone somente números 10 digitos sem espaço e "-"
+$("#telefone").mask("0000000000");
+//mascara CPF 11 digitos sem espaço, "." e "-"
+$("#cpf").mask("00000000000");
+//mascara RG 9 digitos sem espaço,"." e "-"
+$("#rg").mask("000000000");
